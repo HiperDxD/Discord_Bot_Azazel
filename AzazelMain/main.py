fb131a4bd89d7041fcce2ee34ae96f83c6bbe6be
Gherman Armand-Marius (uig20194) <armand.marius.gherman@continental-corporation.com>
2023-07-19 13:01:50 +0300

    Commit: add to bot_token.env DISCORD_SERVER         add to main.py to functionalities to  pull the name of the users on the sv and also to let me know where my bot is connected

--- AzazelMain/main.py
+++ AzazelMain/main.py
@@ -5,11 +5,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER = os.getenv('DISCORD_SERVER')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for server in client.server:
+        if server.name == SERVER:
+            break
+    print(
+        f'{client.user} is connected to the following server:\n'
+        f'{server.name}(id: {server.id})')
+
+    members = '\n - '.join([member.name for member in server.members])
+    print(f'Server Members:\n - {members}')
 
 client.run(TOKEN)
